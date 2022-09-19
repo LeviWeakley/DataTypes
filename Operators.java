e9b33955fa8a4407771dc7ac3e5048f9c47605ed
LeviWeakley <Levi.Weakley@mrjaffesclass.com>
2022-09-19 11:50:33 -0700

    step 4

---
+++ Operators.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class RenameThisClass here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Operators
+{
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int i;
+        double f = 2.5;
+        boolean flag = true;
+        i=10; 
+        f+=i;
+        flag = !flag;
+    
+    }
+}
