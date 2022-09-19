87b10c4a3c450b11c2e71109851bc740cc44b088
LeviWeakley <Levi.Weakley@mrjaffesclass.com>
2022-09-19 11:19:09 -0700

    complete

--- DataTypes.java
+++ DataTypes.java
@@ -5,9 +5,12 @@
  * @author (your name)
  * @version (a version number or a date)
  */
-public class RenameThisClass
+public class DataTypes
 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        int i;
+        double f = 2.5;
+        boolean flag = true;
     }
 }
