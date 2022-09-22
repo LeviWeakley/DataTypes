4fc5823bd8b52da9bb354f620590c6566be9c81a
LeviWeakley <Levi.Weakley@mrjaffesclass.com>
2022-09-22 11:45:03 -0700

    strings

--- Strings.java
+++ Strings.java
@@ -9,5 +9,9 @@ public class Strings
 {
      public static void main(String[] args) {
         System.out.println("Hello World!");
+        String string1="I am a string";
+        String string2=string1;
+        String string3="I am also a string";
+        boolean stringsEqual=string1.equals(string2);
     }
 }
