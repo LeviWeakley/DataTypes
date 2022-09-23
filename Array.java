080b26d58c5f4751e6c26432839912a5a311c389
LeviWeakley <Levi.Weakley@mrjaffesclass.com>
2022-09-23 11:44:49 -0700

    Named

---
+++ Array.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class RenameThisClass here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Array
+{
+    public static void main(String[] args) {
+        String[] strArray={"Alpha","Bravo","Charlie", "Delta","Echo",};
+        
+    }
+}
