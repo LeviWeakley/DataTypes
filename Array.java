96c0fb336939ad958aa2ee1e710b8f7d915e94d8
LeviWeakley <Levi.Weakley@mrjaffesclass.com>
2022-09-26 11:16:16 -0700

    Print

--- Array.java
+++ Array.java
@@ -9,6 +9,9 @@ public class Array
 {
     public static void main(String[] args) {
         String[] strArray={"Alpha","Bravo","Charlie", "Delta","Echo",};
-        
+        for (int i=0; i < strArray.length; i++) {
+            System.out.println(strArray[i]);
+        }
+
     }
 }
